70c4b95070ebfd7a5264b160b8e18e43ab87ff91
Kevin Rivas <kevinsavir20@gmail.com>
2023-10-02 18:18:27 -0300

    se modifica configiración

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-// module.exports = nextConfig
-
-module.exports = {
-    env: {
-        TWILIO_ACCOUNT_SID: "AC1a3b054ddbdf76a0262dc1b19ec9f6d0" ,
-        TWILIO_API_KEY_SID: "SKd1d78aa62657660a73b3df0e4a035dd3",
-        TWILIO_API_KEY_SECRET: "JLswLCRLf6alFZspwCnUEJx2RgSN61Yx",
-    },
-  };
+module.exports = nextConfig
